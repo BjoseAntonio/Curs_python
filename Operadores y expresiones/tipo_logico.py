657bfb00e733bf761b3776136ab3f74a5e57a4de
BjoseAntonio <bas.99jose@gmial.com>
2023-10-19 22:43:08 -0700

    Proyectos HTML_CSs

--- Operadores y expresiones/tipo_logico.py
+++ Operadores y expresiones/tipo_logico.py
@@ -1,1 +1,34 @@
-print("hola mundo")
+# Creamos una expresión mediante un operador para validar el tipo lógico de su resultado (True or False)
+
+n =  1+1 ==3
+m = 1+1 == 2
+print(n)
+print(m)
+print("_____________")
+
+# operadores relacionales 
+# Igual que 
+print(23 == 21)
+# Distinto que
+print(23 != 23) 
+# Mayor que
+print(2 > 1)
+# Menor que 
+print(2 < 1)
+# Mayor o igual que
+print(12 >= 10)
+# Menor o igual que
+print(12 <= 11) 
+print("_____________")
+
+# Validar el valor lógico a través de variables
+a = 2
+b = 4
+print(a == b, 
+      a != b,
+      a <= b)
+print("_____________")
+
+# Validar con caracteres 
+print("Hola" == "Hola",
+      "Hola" != "Mundo")
