e305e03c866e081af962fb9c8d162597127bccdd
Basilio <bas.99jose@gmail.com>
2024-01-04 19:59:59 -0600

    Curs_python

---
+++ 2-Controlando el flujo/1.2-EjerciciosCahtGPT.py
@@ -0,0 +1,26 @@
+print("____________Ejercicio 1___________________")
+# Verificación de Número Par:
+# Problema: Escribe un programa que solicite al usuario ingresar un número e indique si es par o impar.
+# Pista: Utiliza el operador % para verificar si un número es divisible por 2.
+
+
+print("____________Ejercicio 2___________________")
+# Calificación de Examen:
+# Problema: Crea un programa que solicite al usuario ingresar su calificación en un examen (0-100) y muestre un mensaje de aprobación si la calificación es 60 o superior, de lo contrario, un mensaje de reprobación.
+# Pista: Usa una estructura if para comparar la calificación.
+
+
+print("____________Ejercicio3___________________")
+# Categoría de Edad:
+# Problema: Escribe un programa que solicite al usuario ingresar su edad y muestre un mensaje indicando si es un niño (0-12 años), adolescente (13-19 años), adulto (20-59 años) o adulto mayor (60 años en adelante).
+# Pista: Utiliza varias sentencias if y elif para evaluar diferentes rangos de edad.
+
+print("____________Ejercicio 4___________________")
+# Comparación de Números:
+# Problema: Crea un programa que solicite al usuario ingresar dos números y muestre un mensaje indicando cuál de los dos números es mayor, o si son iguales.
+# Pista: Usa la sentencia if para comparar los números.
+
+print("____________Ejercicio 5___________________")
+# Verificación de Número Positivo, Negativo o Cero:
+# Problema: Escribe un programa que solicite al usuario ingresar un número e indique si es positivo, negativo o cero.
+# Pista: Utiliza una combinación de if y else para manejar los diferentes casos.
