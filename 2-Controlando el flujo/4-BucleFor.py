ca113e9bf8da8f11215e2b3209be6820b6f8d6f5
Basilio <bas.99jose@gmail.com>
2024-01-07 21:34:35 -0600

    Curs_python

--- 2-Controlando el flujo/4-BucleFor.py
+++ 2-Controlando el flujo/4-BucleFor.py
@@ -1,24 +1,24 @@
-# numeros = [1,2,3,4,5,6,7,9,10]
+numeros = [1,2,3,4,5,6,7,9,10]
 
-# indice = 0 
+indice = 0 
 
-# for numero in numeros:
-#     print(numero)
+for numero in numeros:
+    print(numero)
 
-# #Para modificar la lista es necesario tomar la lista
-# for numero in numeros:
-#     numeros[indice] *=10
-#     indice += 1
-# print(numeros)
+#Para modificar la lista es necesario tomar la lista
+for numero in numeros:
+    numeros[indice] *=10
+    indice += 1
+print(numeros)
 
 
-# #Otra manera de crear un indice es directo desde el For
+#Otra manera de crear un indice es directo desde el For
 
-# for i, num in enumerate(numeros):
-#     numeros[i] *=100
-# print(numeros)
+for i, num in enumerate(numeros):
+    numeros[i] *=100
+print(numeros)
 
-#Letras 
+# Letras 
 cadena = 'Hola mundo'
 cadena2 = ''
 
@@ -27,4 +27,8 @@ for caracter in cadena:
 
 for caracter in enumerate(cadena):
     cadena2 += '*'
+print(cadena2)
 
+
+for i in range(10):
+    print(i)
