f5c7c5f779518f68976f7c2b9435e987c57b7007
BJoseAntonio <bas.99jose@gmail.com>
2024-01-09 19:52:19 -0600

    Curs_python

--- 2-Controlando el flujo/4.2-EjerciciosChatGPT.py
+++ 2-Controlando el flujo/4.2-EjerciciosChatGPT.py
@@ -2,21 +2,21 @@
 # Problema: Escribe un programa que calcule la suma de los primeros n números pares, donde n es ingresado por el usuario.
 # Pista: Utiliza el operador % para verificar si un número es par.
 
-# numero = int(input("Escribe un numero: "))
-# suma = 0
-# suma_impar = 0
+numero = int(input("Escribe un numero: "))
+suma = 0
+suma_impar = 0
 
-# for i in range(numero+1):
-#   if i % 2 == 0:
-#     suma += i
-#   else:
-#     suma_impar += i
+for i in range(numero+1):
+  if i % 2 == 0:
+    suma += i
+  # else:
+  #   suma_impar += i
 
 
-# print("Suma numero par: ",suma)
+print("Suma numero par: ",suma)
 # print("Suma numero impar: ",suma_impar)
 
-# Tabla de Multiplicar:
+# 2.- Tabla de Multiplicar:
 # Problema: Escribe un programa que solicite al usuario ingresar un número e imprima la tabla de multiplicar de ese número del 1 al 10.
 # Pista: Utiliza un bucle for para iterar del 1 al 10.
 numero_mult = int(input("Ingrese un nunero a multiplcar: "))
@@ -24,4 +24,4 @@ total = 0
 
 for i in range(10+1):
   total = numero_mult * i
-  print(numero_mult," * ", total)
+  print(i," * ",numero_mult," = ", total)
