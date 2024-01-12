3bf5670c04c3bbb04d14dec5858b331e5043ff97
BJoseAntonio <bas.99jose@gmail.com>
2024-01-11 21:25:52 -0600

    Curs_python

---
+++ 2-Controlando el flujo/5.-enumerate.py
@@ -0,0 +1,10 @@
+elementos = ['Hola', 4, 'Adíos', [1,2,3]]
+
+for dupla in enumerate(elementos):
+    print(dupla)
+
+
+for indice, valor in enumerate(elementos):
+    print(indice, valor)
+
+lista_cadena =['Hola', 'Mundo']
