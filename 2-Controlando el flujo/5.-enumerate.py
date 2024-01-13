2cc0f24573756a5331f83a037233f8dfaa1b5ca9
BJoseAntonio <bas.99jose@gmail.com>
2024-01-12 22:57:16 -0600

    Curs_python

--- 2-Controlando el flujo/5.-enumerate.py
+++ 2-Controlando el flujo/5.-enumerate.py
@@ -8,3 +8,7 @@ for indice, valor in enumerate(elementos):
     print(indice, valor)
 
 lista_cadena =['Hola', 'Mundo']
+
+for i,v in enumerate(lista_cadena):
+    lista_cadena[i] = lista_cadena[i][0] 
+    print(lista_cadena)
---
+++ 2-Controlando el flujo/6.-iteraciones_Converciones.py
@@ -0,0 +1,3 @@
+# Pide al usuario que introduzca un número entero por teclado entre 1 y 9 (ambos incluidos) y guárdalo en la variable numero. Debes asegurarte de que esa variable numero contiene un numero entero del 1 al 9, utiliza un bucle para repetir la lectura hasta que se cumpla esa condición.
+
+numero = int(input('Ingrese un numero del 1 al 9: '))
