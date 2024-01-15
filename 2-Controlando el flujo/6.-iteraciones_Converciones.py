59ccacef1ca5e5ff6e3780ff6ff2d5b076b4f7f1
Basilio <bas.99jose@gmail.com>
2024-01-15 17:05:47 -0600

    Curs_python

--- 2-Controlando el flujo/6.-iteraciones_Converciones.py
+++ 2-Controlando el flujo/6.-iteraciones_Converciones.py
@@ -1,3 +1,22 @@
 # Pide al usuario que introduzca un número entero por teclado entre 1 y 9 (ambos incluidos) y guárdalo en la variable numero. Debes asegurarte de que esa variable numero contiene un numero entero del 1 al 9, utiliza un bucle para repetir la lectura hasta que se cumpla esa condición.
+while(True):
+    numero = int(input('Ingrese un numero del 1 al 9: '))
+    if numero >= 1 and numero <= 9:
+        print("El número seleccionado esta dentro del rango de 1 al 9")
+        break
+    else:
+        print("Intente otra ves")
 
-numero = int(input('Ingrese un numero del 1 al 9: '))
+
+# Genera una lista llamada multiplos que contenga los múltiplos de numero en el rango de 1 a 100 (ambos incluidos) utilizando la conversión de un range a list con un paso: list(range(Min,Max,Paso)).
+        
+multiplos = []
+
+numero_multiplo = int(input("Ingrese un numero: "))
+
+for i,v in  list(range(numero_multiplo,101, numero_multiplo)):
+  print(i)
+#   multiplos[i] = i
+
+# print(multiplos)
+
